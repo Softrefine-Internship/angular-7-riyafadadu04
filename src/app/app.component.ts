18861a2b8fe19fee5ed1656792295bd53b3b2a28
riyafadadu04 <riyafadadu2004@gmail.com>
2025-01-20 18:30:03 +0530

    create basic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
 
+interface dynamicFormJson {
+  type: string;
+  label: string;
+  isVisible: boolean;
+  isRequired: boolean;
+  errorMessage: string;
+  hint: string;
+  value: string | boolean | number;
+  options?: (string | boolean | number)[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-7';
+  formJson: dynamicFormJson[] = [
+    {
+      type: 'text',
+      label: 'Name',
+      isVisible: true,
+      isRequired: true,
+      errorMessage: 'Enter valid name',
+      hint: 'Enter your full name',
+      value: '',
+    },
+    {
+      type: 'number',
+      label: 'Age',
+      isVisible: true,
+      isRequired: true,
+      errorMessage: 'Enter valid Age',
+      hint: '',
+      value: '',
+    },
+    {
+      type: 'range',
+      label: 'Rate',
+      isVisible: true,
+      isRequired: false,
+      errorMessage: 'Enter valid Range',
+      hint: '',
+      value: 5,
+      options: [1, 10],
+    },
+    {
+      type: 'dropdown',
+      label: 'Gender',
+      isVisible: true,
+      isRequired: true,
+      errorMessage: 'Gender is required',
+      hint: 'Select your gender',
+      value: '',
+      options: ['Male', 'Female'],
+    },
+    {
+      type: 'checkbox',
+      label: 'agree',
+      isVisible: true,
+      isRequired: true,
+      errorMessage: 'You must agree to terms',
+      hint: '',
+      value: false,
+    },
+    {
+      type: 'textarea',
+      label: 'address',
+      isVisible: true,
+      isRequired: false,
+      errorMessage: '',
+      hint: 'Enter Your Address',
+      value: '',
+    },
+  ];
 }
